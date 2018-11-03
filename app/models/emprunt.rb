2b0c9a461aa750ec6df7e5853e1ff2cfd7a98fed
Eric Foucault <efoucault@gmail.com>
2018-11-03 12:06:18 +0100

    ajout de mes emprunts sur mon profil

--- app/models/emprunt.rb
+++ app/models/emprunt.rb
@@ -1,4 +1,7 @@
 class Emprunt < ApplicationRecord
   belongs_to :user
   belongs_to :livre
+  validates :note, numericality: { greater_than: 0 }
+  validates :note, numericality: { less_than: 6 }
+
 end
