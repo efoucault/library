1d75c32c0424d22870976d90bcba162b29823226
Eric Foucault <efoucault@gmail.com>
2018-11-13 22:32:01 +0100

    envoi d'un mail au propriétaire du livre lors d'un emprunt

---
+++ app/mailers/emprunt_mailer.rb
@@ -0,0 +1,14 @@
+class EmpruntMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.emprunt_mailer.emprunt.subject
+  #
+  def emprunt(emprunt)
+    @user = emprunt.user
+    @livre = emprunt.livre
+
+    mail(to: @livre.user.email, subject: 'Ton livre a été emprunté')
+  end
+end
