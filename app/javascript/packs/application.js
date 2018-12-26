0eb48fcde70fee4baef28c6f9453a2401dffd904
Eric Foucault <efoucault@gmail.com>
2018-12-26 12:44:14 +0100

    découpage du profil user avec affichage conditionnel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -43,45 +43,100 @@ wishlistButtons.forEach((button) => {
   });
 });
 
+// Profil User
+const MesLivres = document.querySelector(".mes-livres");
+const MesEmprunts = document.querySelector(".mes-emprunts");
+const MesNotifs = document.querySelector(".notifications");
+const MaWishlist = document.querySelector(".wishlist");
+
+if (MesLivres != null) {
+  const LivreIcon = document.querySelector(".fa-book");
+  LivreIcon.addEventListener("click", (event) => {
+    MesLivres.classList.remove("hidden");
+    MesEmprunts.classList.add("hidden");
+    MesNotifs.classList.add("hidden");
+    MaWishlist.classList.add("hidden");
+    LivreIcon.classList.add("red");
+    GlassesIcon.classList.remove("red");
+    HeartIcon.classList.remove("red");
+    EnvelopeIcon.classList.remove("red");
+  });
+  const GlassesIcon = document.querySelector(".fa-glasses");
+  GlassesIcon.addEventListener("click", (event) => {
+    MesLivres.classList.add("hidden");
+    MesEmprunts.classList.remove("hidden");
+    MesNotifs.classList.add("hidden");
+    MaWishlist.classList.add("hidden");
+    LivreIcon.classList.remove("red");
+    GlassesIcon.classList.add("red");
+    HeartIcon.classList.remove("red");
+    EnvelopeIcon.classList.remove("red");
+  });
+  const HeartIcon = document.querySelector(".fa-heart");
+  HeartIcon.addEventListener("click", (event) => {
+    MesLivres.classList.add("hidden");
+    MesEmprunts.classList.add("hidden");
+    MesNotifs.classList.add("hidden");
+    MaWishlist.classList.remove("hidden");
+    LivreIcon.classList.remove("red");
+    GlassesIcon.classList.remove("red");
+    HeartIcon.classList.add("red");
+    EnvelopeIcon.classList.remove("red");
+  });
+  const EnvelopeIcon = document.querySelector(".fa-envelope");
+  EnvelopeIcon.addEventListener("click", (event) => {
+    MesLivres.classList.add("hidden");
+    MesEmprunts.classList.add("hidden");
+    MesNotifs.classList.remove("hidden");
+    MaWishlist.classList.add("hidden");
+    LivreIcon.classList.remove("red");
+    GlassesIcon.classList.remove("red");
+    HeartIcon.classList.remove("red");
+    EnvelopeIcon.classList.add("red");
+  });
+}
+
 const resultats = document.querySelector(".resultats");
 const bookKey = document.querySelector('.googlebook_key').dataset.google;
 const contenu = document.querySelector("#livre_titre");
 
-contenu.addEventListener('blur', (evt) => {
-  fetch(`https://www.googleapis.com/books/v1/volumes?q=${contenu.value}&key=${bookKey}`, {
-     method: "GET",
-     })
-     .then(response => response.json())
-     .then((data) => {
-      const resultats = document.querySelector(".resultats");
-      resultats.classList.remove("hidden");
-      const Ensembleresultats = document.querySelectorAll(".resultat");
-      Ensembleresultats.forEach((resultat) => {
-        resultat.parentNode.removeChild(resultat);
-      });
-       var compteur;
-       for (compteur = 2; compteur >= 0; compteur--) {
-         const titre = data.items[compteur].volumeInfo.title;
-         const auteur = data.items[compteur].volumeInfo.authors[0];
-         var thumbnail = "https://books.google.fr/googlebooks/images/no_cover_thumb.gif"
-         if (data.items[compteur].volumeInfo.imageLinks !== undefined) {
-           thumbnail = data.items[compteur].volumeInfo.imageLinks.thumbnail;
-         }
-         resultats.insertAdjacentHTML("afterbegin", `<div class="resultat" data-compteur="${compteur}"> \n
-            <div class="livre-couverture"> \n
-              <img src="${thumbnail}" width="50%" height="80%" alt=""> \n
+if (contenu != null) {
+  contenu.addEventListener('blur', (evt) => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${contenu.value}&key=${bookKey}`, {
+       method: "GET",
+       })
+       .then(response => response.json())
+       .then((data) => {
+        const resultats = document.querySelector(".resultats");
+        resultats.classList.remove("hidden");
+        const Ensembleresultats = document.querySelectorAll(".resultat");
+        Ensembleresultats.forEach((resultat) => {
+          resultat.parentNode.removeChild(resultat);
+        });
+         var compteur;
+         for (compteur = 2; compteur >= 0; compteur--) {
+           const titre = data.items[compteur].volumeInfo.title;
+           const auteur = data.items[compteur].volumeInfo.authors[0];
+           var thumbnail = "https://books.google.fr/googlebooks/images/no_cover_thumb.gif"
+           if (data.items[compteur].volumeInfo.imageLinks !== undefined) {
+             thumbnail = data.items[compteur].volumeInfo.imageLinks.thumbnail;
+           }
+           resultats.insertAdjacentHTML("afterbegin", `<div class="resultat" data-compteur="${compteur}"> \n
+              <div class="livre-couverture"> \n
+                <img src="${thumbnail}" width="50%" height="80%" alt=""> \n
+              </div> \n
+              <div class="details"> \n
+                <p><strong>${titre}</strong></p> \n
+                <p id="auteur">${auteur}</p> \n
+              </div> \n
             </div> \n
-            <div class="details"> \n
-              <p><strong>${titre}</strong></p> \n
-              <p id="auteur">${auteur}</p> \n
-            </div> \n
-          </div> \n
-          `);
+            `);
 
-         ClickItem(data);
-    }
+           ClickItem(data);
+      }
+    });
   });
-});
+}
 
 // // Get Book thumbnail
 // function bookThumbnail(data, compteur) {
@@ -137,5 +192,6 @@ if (cameraIcon != null && preview != null) {
       reader.readAsDataURL(videoCachee.files[0]);
   });
 }
+
 
 
